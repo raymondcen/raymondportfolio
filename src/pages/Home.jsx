7d932aa0f9353e7d2d4756a897be28fcb918cdb4
raymondcen <raymondcen07@gmail.com>
2025-01-19 23:53:32 -0800

    updated home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
 const Home = () => {
-  return (
-    <div className="inset-x-0 top-0 absolute bg-red-400 h-[1920px]
-                    md:ml-[30vw] md:mr-[20vw]">
+  return ( 
+    // greenbox
+    <div className="inset-x-0 top-0 absolute bg-emerald-100 h-[1920px] resize
+                    md:mx-[10vw] md:my-[58vh]">
 
-      {/* green box visual */}
-      <div className="inset-x-0 top-0 absolute bg-emerald-100 h-[1920px] resize
-                    md:-ml-[20vw] md:-mr-[10vw] md:my-[58vh]">
+     
+      <div className="inset-x-0 top-0 absolute bg-red-400 h-[1920px]
+                    md:ml-[20vw] md:mr-[10vw] md:-my-[58vh]">
+          <div className="mt-[58vh]"> 
+            <h1>
+              Welcome to the Home Page AA A A A AA A A A  A A A AA A A A  
+              AA A A HH H H H HH H H H HH HHH H H H HH H HH H a a aa a a a 
+              aa a a aa a a a aa a aaaa a aa a a a aa a a aa a a a aa a a aa  
+              a a a a a! 
+            </h1>
+          </div>
       </div>
-
-      
-       <div className=""> 
-        <h1>Welcome to the Home Page AA A A A AA A A A  A A A AA A A A  
-          AA A A HH H H H HH H H H HH HHH H H H HH H HH H a a aa a a a 
-          aa a a aa a a a aa a aaaa a aa a a a aa a a aa a a a aa a a aa  
-          a a a a a! </h1>
-        </div>
+       
     </div>
   );
 };
